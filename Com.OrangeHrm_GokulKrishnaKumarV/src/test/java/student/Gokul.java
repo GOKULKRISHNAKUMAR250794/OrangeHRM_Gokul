a2f4b6fba8eed589732087cfb26f2740df94fb3c
Gokul Krishna kumar <gokulkrishnakumarv@gmail.com>
2025-10-28 13:36:14 +0530

    i added after pull

--- Com.OrangeHrm_GokulKrishnaKumarV/src/test/java/student/Gokul.java
+++ Com.OrangeHrm_GokulKrishnaKumarV/src/test/java/student/Gokul.java
@@ -6,6 +6,7 @@ public class Gokul {
 		System.out.println("Hi I Am Gokul");
 		System.out.println("yes I am");
 		System.out.println("need to pull this changes before push to my account");
+		System.out.println("i need to add my changes after pull");
 	}
 
 }
