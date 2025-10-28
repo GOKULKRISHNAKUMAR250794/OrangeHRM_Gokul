11b987fc8b5b5327f9f89094495afe357b46e963
GOKULKRISHNAKUMAR250794 <gokulkrishnakumarv@gmail.com>
2025-10-28 13:28:06 +0530

    Update Gokul.java

--- Com.OrangeHrm_GokulKrishnaKumarV/src/test/java/student/Gokul.java
+++ Com.OrangeHrm_GokulKrishnaKumarV/src/test/java/student/Gokul.java
@@ -5,6 +5,7 @@ public class Gokul {
 	public static void main(String[] args) {
 		System.out.println("Hi I Am Gokul");
 		System.out.println("yes I am");
+		System.out.println("need to pull this changes before push to my account");
 	}
 
 }
